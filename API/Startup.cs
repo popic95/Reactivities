ecea770ed3858dd657e7ae72b2d64ddc590e6e80
Vladut <ppa.vladut@gmail.com>
2021-11-24 19:15:20 +0200

    End of section 10

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,9 @@
 using API.Extensions;
+using API.Middleware;
 using Application.Activities;
 using Application.Core;
 using AutoMapper;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -30,7 +32,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
 
         }
@@ -38,9 +43,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Domain
 {
-    public class Activity
+    public class Activity 
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
