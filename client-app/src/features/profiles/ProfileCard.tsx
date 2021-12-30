c80464d3ae465280b19a0d44e4193d35c006192d
Vladut <ppa.vladut@gmail.com>
2021-12-31 00:59:48 +0200

    End of section 18

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -4,21 +4,25 @@ import { Link } from "react-router-dom";
 import { Card, Icon, Image } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 
-interface Props{
+interface Props {
     profile: Profile;
 }
 
-export default observer(function ProfileCard({profile}:Props){
-
+export default observer(function ProfileCard({ profile }: Props) {
+    function truncate(str: string | undefined) {
+        if (str) {
+            return str.length > 40 ? str.substring(0, 37) + '...' : str;
+        }
+    }
     return (
         <Card as={Link} to={`/profiles/${profile.username}`}>
             <Image src={profile.image || '/assets/user.png'} />
             <Card.Content>
                 <Card.Header>{profile.displayName}</Card.Header>
-                <Card.Description>Bio goes here</Card.Description>
+                <Card.Description>{profile.bio}</Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <Icon name='user'/>
+                <Icon name='user' />
                 20 followers
             </Card.Content>
         </Card>
